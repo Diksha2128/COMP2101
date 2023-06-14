7b96b19beadcf9afc7adc9a3d988afe53d9ea0eb
Diksha <rdishu41@gmail.com>
2023-06-14 17:23:11 -0400

    Using Cat

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
-#Main Script
 
+echo "My second script"
+#dat1 is a variable which will give the hostname
 
+dat1=$(hostname)
 
+#dat2 is a variable which will give the fully qualified domain name(FQDN)
 
-echo "FQDN: $(hostname -I)"
-echo "Host Information"
-hostnamectl
-echo "IP adresses:"
-hostname -I
-echo "Root Filesystem Status:"
-df -h /
+dat2=$(hostname -f)
+
+#dat 3 will give the whole information regarding the system and grep will trim it operating system information
+
+dat3=$(hostnamectl | grep Operating | awk '{print $3,$4,$5}')
+
+#dat4  is a variable which will provide the IP address of the host name
+
+dat4=$(hostname -I)
+
+#dat5 is a variable which will give us the system indormation in which awk is used to give only the fourth line and then tail is used because we only want the last line
+
+dat5=$(df -h / | awk '{print $4}' | tail -n 1)
+
+cat <<EOF
+Report for:$dat1
+===============
+FQDN:$dat2
+Operating System name and version:$dat3
+IP Address:$dat4
+Root Filesystem Free Space:$dat5
+===============
+EOF
