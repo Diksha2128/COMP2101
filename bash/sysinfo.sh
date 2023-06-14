96fab75c4f664ac4eddfcc6c849e41b38153c356
Diksha <rdishu41@gmail.com>
2023-06-14 18:09:15 -0400

    Lab3

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,32 +1,69 @@
 #!/bin/bash
 
-echo "My second script"
-#dat1 is a variable which will give the hostname
+# Check if the user has root privilege
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit 1
+fi
 
-dat1=$(hostname)
+# Function to check command success and print error message if failed
+check_command() {
+  if [ $? -ne 0 ]; then
+    echo "Error: Failed to retrieve $1."
+  fi
+}
 
-#dat2 is a variable which will give the fully qualified domain name(FQDN)
+# Section: System Description
+echo "=== System Description ==="
+manufacturer=$(dmidecode -s system-manufacturer)
+check_command "system manufacturer"
+echo "Computer manufacturer: $manufacturer"
 
-dat2=$(hostname -f)
+model=$(dmidecode -s system-product-name)
+check_command "system model"
+echo "Computer description or model: $model"
 
-#dat 3 will give the whole information regarding the system and grep will trim it operating system information
+serial=$(dmidecode -s system-serial-number)
+check_command "system serial number"
+echo "Computer serial number: $serial"
 
-dat3=$(hostnamectl | grep Operating | awk '{print $3,$4,$5}')
+# Section: CPU Information
+echo -e "\n=== CPU Information ==="
+cpu_manufacturer=$(lshw -class processor | awk '/product/ {print $2}' | head -n 1)
+check_command "CPU manufacturer"
+echo "CPU manufacturer and model: $cpu_manufacturer"
 
-#dat4  is a variable which will provide the IP address of the host name
+cpu_arch=$(lshw -class processor | awk '/capabilities/ {print $3}' | head -n 1)
+check_command "CPU architecture"
+echo "CPU architecture: $cpu_arch"
 
-dat4=$(hostname -I)
+cpu_cores=$(lshw -class processor | awk '/core/ {print $2}' | head -n 1)
+check_command "CPU core count"
+echo "CPU core count: $cpu_cores"
 
-#dat5 is a variable which will give us the system indormation in which awk is used to give only the fourth line and then tail is used because we only want the last line
+cpu_max_speed=$(lshw -class processor | awk '/capacity/ {print $2}' | head -n 1)
+check_command "CPU maximum speed"
+echo "CPU maximum speed: $cpu_max_speed Hz"
 
-dat5=$(df -h / | awk '{print $4}' | tail -n 1)
+cache_sizes=$(lshw -class cache | awk '/size/ {print $2}' | head -n 3)
+check_command "cache sizes"
 
-cat <<EOF
-Report for:$dat1
-===============
-FQDN:$dat2
-Operating System name and version:$dat3
-IP Address:$dat4
-Root Filesystem Free Space:$dat5
-===============
-EOF
+cache_sizes_arr=($cache_sizes)
+l1_cache=${cache_sizes_arr[0]}
+l2_cache=${cache_sizes_arr[1]}
+l3_cache=${cache_sizes_arr[2]}
+
+echo "Sizes of caches (L1, L2, L3):"
+echo "L1 cache size: $l1_cache"
+echo "L2 cache size: $l2_cache"
+echo "L3 cache size: $l3_cache"
+
+# Section: Operating System Information
+echo -e "\n=== Operating System Information ==="
+distro=$(lsb_release -ds)
+check_command "Linux distro"
+echo "Linux distro: $distro"
+
+distro_version=$(lsb_release -rs)
+check_command "distro version"
+echo "Distro version: $distro_version"
